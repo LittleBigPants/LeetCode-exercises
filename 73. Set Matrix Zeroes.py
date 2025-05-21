d516580d62467f12b91bc78534ff7c2a22d475a5
Juan Manuel Garcia <juanmanuel_garcia98@hotmail.com>
2025-05-21 17:14:54 -0300

    Create 73. Set Matrix Zeroes.py

---
+++ 73. Set Matrix Zeroes.py
@@ -0,0 +1,22 @@
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        colums = len(matrix)
+        rows = len(matrix[0])
+        x= []
+        y= []
+        dic= {}
+        for i in range(colums):
+                for e in range(rows):
+                    if matrix[i][e] == 0:
+                        x.append(e)
+                        y.append(i)
+        for fila in y:
+            for col in range(rows):
+                matrix[fila][col] = 0
+        for col in x:
+            for fila in range(colums):
+                matrix[fila][col] = 0 
+        
