ba63f4e97cafb360757b52424c3ae086e1fb78c4
Juan Manuel Garcia <juanmanuel_garcia98@hotmail.com>
2025-05-22 16:38:49 -0300

    Update 27. Remove Element.py

---
+++ 27. Remove Element.py
@@ -0,0 +1,14 @@
+from typing import List
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        k = len(nums) -1
+        i = 0
+        while i<= k:
+            if nums[i] == val:
+                nums[i], nums[k] = nums[k], nums[i]
+                nums.pop()
+                k -= 1
+            else:
+                i += 1
+
+        print(nums)
